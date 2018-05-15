6ca7de9a3ef89196e3a31a7c76888e8bb341000f
Simon Morley <simon@polkaspots.com>
2018-05-15 14:31:08 +0100

    More stuff

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 REDIS_URL = os.environ.get("REDIS_URL")
 SENDGRID_TOKEN = os.environ.get("SENDGRID_TOKEN")
-NEW_LIST = 3790322
-OLD_LIST = 3021445
+NEW_LIST = os.environ.get("NEW_LIST") #3790323
+OLD_LIST = os.environ.get("OLD_LIST") #3790323
